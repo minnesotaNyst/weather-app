8b13453b2f56c1ba1f4b8a42fc955d39f50a44eb
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-11-08 20:11:48 -0600

    moving on to the five day forecast, couldn't get the display history to work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,22 @@
 /* $( document ).ready( function () { */
-var url = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+var wUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+var uUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=';
 var appid = '&units=imperial&appid=';
 var key = '21d337020a0247b874a0d43202c4ad83';
 var weather = '';
 var cities = [];
 
+//function to display search history in the left column of the app
 /* var searchHistory = function () {
-	cities = JSON.parse(localStorage.getItem('search-history'));
+	cities = JSON.parse(localStorage.getItem('cities'));
 	console.log(cities);
 
 	if (!cities) {
 		cities = [];
+		return;
 	} else {
-		for (var i = 0; i < history.length; i++) {
-			var historyEl = $('<p>');
+		for (var i = 0; i < cities.length; ++i) {
+			var historyEl = $('search-history').add('p');
 			historyEl.text(cities[i]);
 			historyEl.addClass('list-group-item btn btn-light');
 			$('#search-history').append(historyEl);
@@ -21,9 +24,9 @@ var cities = [];
 	}
 }; */
 
-// When user searches for a city, hit the API
+//function to grab the searched for city and return required data elements
 var getWeather = function (city) {
-	fetch(url + city + appid + key).then(function (response) {
+	fetch(wUrl + city + appid + key).then(function (response) {
 		if (response.ok) {
 			response.json().then(function (data) {
 				var lat = data.city.coord.lat;
@@ -32,28 +35,24 @@ var getWeather = function (city) {
 				console.log(lat, lon, location);
 				var currentDate = moment().format('MM/DD/YYYY');
 				$('#current-city').text(location + ' ' + currentDate);
-				/* $('#weather-img').attr('src', url + json.data.weather[0].icon + '.png'); */
+				/* $('#weather-img').attr('src', wUrl + json.data.weather[0].icon + '.png'); */
 				$('#temperature').text(data.list[0].main.temp.toFixed(0) + '°F');
 				$('#temp_low').text(data.list[0].main.temp_min.toFixed(0) + '°F');
 				$('#temp_high').text(data.list[0].main.temp_max.toFixed(0) + '°F');
 				$('#humidity').text(data.list[0].main.humidity + '%');
 				$('#windspeed').text(data.list[0].wind.speed.toFixed(0) + ' ' + 'mph');
+				setHistory(location);
 				/* getForecast(lat, lon); */
 
-				fetch(
-					'https://api.openweathermap.org/data/2.5/onecall?lat=' +
-						lat +
-						'&lon=' +
-						lon +
-						'&appid=' +
-						key
-				).then(function (response) {
+				fetch(uUrl + lat + '&lon=' + lon + '&appid=' + key).then(function (
+					response
+				) {
 					if (response.ok) {
 						response.json().then(function (data) {
 							$('#uvIndex').text(data.current.uvi);
 							console.log(data.current.uvi);
 							if (data.current.uvi > 2 && data.current.uvi < 5) {
-                                //come back to this and figure out the addclass method to change the warning
+								//come back to this and figure out the addclass method to change the warning
 								$('#uvIndex').css('bg-warning');
 							} else if (data.current.uvi < 2) {
 								$('uvIndex').css('bg-success');
@@ -75,53 +74,22 @@ var getWeather = function (city) {
 };
 
 // Fetch forecast data from weather API (use the onecall api here)
-/* var getForecast = function (city) {
-	fetch(url + city + appid + key).then(function (response) {
+var getForecast = function (lat, lon) {
+	fetch(uUrl + lat + '&lon=' + lon + '&appid=' + key).then(function (response) {
 		// display forecast data from API
 		if (response.ok) {
-			for (var i = 0; i < response.list.length; i++) {
-				var dateTime = response.list[i].dt_txt;
-				var date = dateTime.split(' ')[0];
-				var time = dateTime.split(' ')[1];
-				if (time === '15:00:00') {
-					var year = date.split('-')[0];
-					var month = date.split('-')[1];
-					var day = date.split('-')[2];
-					$('#day-' + dayCounter)
-						.children('.card-date')
-						.text(month + '/' + day + '/' + year);
-					$('#day-' + dayCounter)
-						.children('.weather-icon')
-						.attr(
-							'src',
-							'https://api.openweathermap.org/img/w/' +
-								response.list[i].weather[i].icon +
-								'.png'
-						);
-					$('#day-' + dayCounter)
-						.children('.weather-temp')
-						.text('Temp: ' + (response.list[i].main.temp.toFixed(0) + '°F'));
-					$('#day-' + dayCounter)
-						.children('.weather-humidity')
-						.text('Humidity: ' + response.list[i].main.humidity + '%');
-					dayCounter++;
-				}
-			}
-		} else {
-			alert(
-				'Please make sure the city you are searching for is spelled correctly!'
-			); //could this be a modal?
-		}
-	});
-}; */
+            response.json().then(function(data){
 
-//function to get the city (working)
+            })
+	});
+};
+
+//function to get the city
 var getCity = function (e) {
 	e.preventDefault();
 	//create a new variable for city to pass into the getForecast function
 	var cityEl = $('#city');
 	var city = $.trim(cityEl.val());
-	console.log(city);
 
 	if (city) {
 		getWeather(city);
@@ -133,10 +101,29 @@ var getCity = function (e) {
 	console.log(e);
 };
 
-/* $('.btn').click(getWeather);
-$('.btn').click(getForecast);
+//function to set the history for cities searched
+var setHistory = function (location) {
+	if (cities.includes(location)) {
+		return;
+	} else {
+		//if a location does exist, then push the value to the cities variable
+		cities.push(location);
+	}
 
-$('#search-city').click(searchHistory);
-searchHistory(); */
+	//ths will put the most recent 8 entries at the top of the list
+	if (cities.length > 8) {
+		cityHistory.shift();
+	}
+
+	//set the local storage with a key name of cities
+	localStorage.setItem('cities', JSON.stringify(cities));
+
+	$('cities').each(function () {
+		$(this).remove();
+	});
+
+	/* 	searchHistory(); */
+};
 
 $('#submit').on('click', getCity);
+/* searchHistory(); */
